2c6ae633e8f0a615f93a00ac02f08d3848cac11d
Michael Simmons <msimmons86@gmail.com>
2022-04-03 15:55:56 -0700

    commit tree 27bba90aadd552695cac6d838cf80c5827fd9109

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+//function superbowlWin(win){
+  //const winningYear = win.find(win => win.result ==="W")
+  //return win.year
+//}
+
+//function superbowlWin2(data){
+ // if (data.result === "W"){
+  //  return record.year
+ // }
+//}
+
+//function superbowlWin(winningYear){
+  //return winningYear.record === "W"
+//}
+// console.log(record.find(WiningYear))
+
+function superbowlWin(element){
+  const result = element.find(record => record.result === "W")
+    return (result ? result.year : undefined)
+}
